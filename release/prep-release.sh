d89e1bba995dd3287f00254de7bc1d3596db13e4
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2019-10-15 23:32:15 -0400

    switching to using DMG canvas to preserve code signatures
    also fixing line endings

--- release/prep-release.sh
+++ release/prep-release.sh
@@ -4,13 +4,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_NAME="photon_metronome"
-BUILD_DIR="${DIR}/${PROJECT_NAME}/application.macosx"
+BUILD_TARGET="application.macosx"
+BUILD_DIR="${DIR}/../src/${PROJECT_NAME}/${BUILD_TARGET}"
 APP="${BUILD_DIR}/${PROJECT_NAME}.app"
 CONFIG="config.properties"
 PLIST="${APP}/Contents/Info.plist"
 VERSION='0.1-beta.3'
 COPYRIGHT='Copyright © 2019 Krishna Bhamidipati. All rights reserved.'
 CERT_IDENITY="Developer ID Application: Krishna Bhamidipati (V2H2HR4VR9)"
+DMG_INITIAL_SIZE="300m"
 
 
 # gatekeeper ftw
@@ -20,14 +22,6 @@ codesign --sign "${CERT_IDENITY}" "${APP}"
 
 # use SHA1 hash as a pseudo-build number
 BUILD_NUMBER=`shasum "${APP}/Contents/MacOS/${PROJECT_NAME}" | head -c 6`
-# prepare zip file name
-ZIP="${DIR}/${PROJECT_NAME}_v${VERSION}-macosx-${BUILD_NUMBER}.zip"
-
-
-# externalize config via symlink for easy editing
-# properties file need still be bundled inside app for proper sandboxing
-if [ -f "${BUILD_DIR}/${CONFIG}" ]; then rm "${BUILD_DIR}/${CONFIG}"; fi
-ln -s "${APP}/Contents/Java/data/${CONFIG}" "${BUILD_DIR}/${CONFIG}"
 
 
 # write plist metadata
@@ -36,7 +30,16 @@ defaults write "${PLIST}" NSHumanReadableCopyright "${COPYRIGHT}"
 defaults write "${PLIST}" CFBundleVersion "${BUILD_NUMBER}"
 
 
-# zip up folder
-# use ditto because it compresses folders exactly like Finder
-# whereas zip is unwiedly with parent folders
-ditto -c -k --sequesterRsrc "${BUILD_DIR}" "${ZIP}"
+# prepare package for DMG Canvas
+# use dmg because it always preserves code signatures unlike zip, tar and ditto
+DMG_DIR="${DIR}/Photon Metronome"
+mkdir -p "${DMG_DIR}"
+cp -r "${APP}" "${DMG_DIR}/"
+
+# externalize config via symlink for easy editing
+# properties file need still be bundled inside app for proper sandboxing
+if [ -f "${DMG_DIR}/${CONFIG}" ]; then rm "${DMG_DIR}/${CONFIG}"; fi
+# use relative path for symlink
+cd "${DMG_DIR}"
+ln -s "${PROJECT_NAME}.app/Contents/Java/data/${CONFIG}" "${CONFIG}"
+cd -
